c404810950ecf5bef2f07ec55973eee24e0b1859
Sagarrokade006 <sagarrokade006@gmail.com>
2019-01-09 11:13:22 +0000

    Update Versioning & Envsetup.sh

--- config/version.mk
+++ config/version.mk
@@ -1,36 +1,24 @@
-# Copyright (C) 2017 AospExtended ROM
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-PRODUCT_BRAND ?= Pearl
-
-# Versioning System
-# Lean version over here.
-PRODUCT_VERSION_MAJOR = v1.0
-PRODUCT_VERSION_MINOR = SparkyWhite
-PEARL_POSTFIX := -$(shell date +"%Y%m%d")
+#Pearl version.
+PRODUCT_VERSION_MAJOR = 9.0
+PRODUCT_VERSION_MAINTENANCE = SparkyWhite
+PEARL_POSTFIX := $(shell date +"%Y%m%d")
+PEARL_BUILD_EXTRA := By-Team-Pearl
 
 ifndef PEARL_BUILD_TYPE
-    PEARL_BUILD_TYPE := Unofficial
+    PEARL_BUILD_TYPE := UNOFFICIAL
 endif
 
-ifdef PEARL_BUILD_EXTRA
-    PEARL_POSTFIX := -$(PEARL_BUILD_EXTRA)
-    PEARL_MOD_SHORT := PearlOS-$(PRODUCT_VERSION_MAJOR)-$(PEARL_BUILD)-$(PEARL_BUILD_TYPE)$(PEARL_POSTFIX)
-else
-    PEARL_MOD_SHORT := PearlOS-$(PRODUCT_VERSION_MAJOR)-$(PEARL_BUILD)-$(PEARL_BUILD_TYPE)
-endif
+# Set all versions
+PEARL_VERSION := PearlOS-$(PEARL_BUILD)-$(PRODUCT_VERSION_MAINTENANCE)-$(PEARL_BUILD_TYPE)-$(PEARL_POSTFIX)
+PEARL_MOD_VERSION := PearlOS-$(PEARL_BUILD)-$(PRODUCT_VERSION_MAINTENANCE)-$(PEARL_BUILD_TYPE)-$(PEARL_POSTFIX)
+PEARL_XTRA_VERSION := $(PRODUCT_VERSION_MAINTENANCE)-$(PEARL_BUILD_TYPE)
+PEARL_DISPLAY_VERSION := $(PEARL_XTRA_VERSION)
 
-PEARL_VERSION := PearlOS-$(PRODUCT_VERSION_MAJOR)-$(PEARL_BUILD)-$(PEARL_BUILD_TYPE)$(PEARL_POSTFIX)
-
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+PRODUCT_PROPERTY_OVERRIDES += \
+    BUILD_DISPLAY_ID=$(BUILD_ID) \
+    pearl.ota.version=$(PRODUCT_VERSION_MAJOR)-$(PRODUCT_VERSION_MAINTENANCE) \
+    ro.pearl.version=$(PEARL_XTRA_VERSION) \
+    ro.modversion=$(PEARL_MOD_VERSION) \
+    ro.pearl.buildtype=$(PEARL_BUILD_TYPE) \
+    ro.pearl.display.version=$(PEARL_DISPLAY_VERSION) \
+    com.pearl.fingerprint=$(PEARL_XTRA_VERSION)
