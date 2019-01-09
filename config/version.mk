38ed798bb391032a21e8604ebc3fafaf93c1a6cd
Sagarrokade006 <sagarrokade006@gmail.com>
2019-01-09 11:29:02 +0000

    Update Versioning

--- config/version.mk
+++ config/version.mk
@@ -1,31 +1,37 @@
-#Pearl version.
-PRODUCT_VERSION_MAJOR = 9.0
-PRODUCT_VERSION_MAINTENANCE = SparkyWhite
-PEARL_POSTFIX := $(shell date +"%Y%m%d")
-PEARL_BUILD_EXTRA := By-Team-Pearl
+# Copyright (C) 2018 PearlOS
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-#UNOFFICIAL
+PEARL_MOD_VERSION = 9.0
+
 ifndef PEARL_BUILD_TYPE
     PEARL_BUILD_TYPE := UNOFFICIAL
 endif
 
-# Beta Tag
 ifeq ($(PEARL_BETA),true)
     PEARL_BUILD_TYPE := BETA
 endif
 
-# Alpha Tag
 ifeq ($(PEARL_ALPHA),true)
-   PEARL_BUILD_TYPE := ALPHA
+    PEARL_BUILD_TYPE := ALPHA
 endif
 
-# Specific Official Tag
 CURRENT_DEVICE=$(shell echo "$(TARGET_PRODUCT)" | cut -d'_' -f 2,3)
 
 ifeq ($(PEARL_OFFICIAL),true)
    LIST = $(shell curl -s https://raw.githubusercontent.com/PearlOS/vendor_pearl/pie/pearl.devices)
    FOUND_DEVICE = $(filter $(CURRENT_DEVICE), $(LIST))
-   ifeq ($(FOUND_DEVICE),$(CURRENT_DEVICE))
+    ifeq ($(FOUND_DEVICE),$(CURRENT_DEVICE))
       IS_OFFICIAL=true
       PEARL_BUILD_TYPE := OFFICIAL
    else
@@ -33,17 +39,14 @@ ifeq ($(PEARL_OFFICIAL),true)
    endif
 endif
 
-# Set all versions
-PEARL_VERSION := PearlOS-$(PEARL_BUILD)-$(PRODUCT_VERSION_MAINTENANCE)-$(PEARL_BUILD_TYPE)-$(PEARL_POSTFIX)
-PEARL_MOD_VERSION := PearlOS-$(PEARL_BUILD)-$(PRODUCT_VERSION_MAINTENANCE)-$(PEARL_BUILD_TYPE)-$(PEARL_POSTFIX)
-PEARL_XTRA_VERSION := $(PRODUCT_VERSION_MAINTENANCE)-$(PEARL_BUILD_TYPE)
-PEARL_DISPLAY_VERSION := $(PEARL_XTRA_VERSION)
+PEARL_VERSION := PearlOS-$(PEARL_MOD_VERSION)-$(CURRENT_DEVICE)-$(PEARL_BUILD_TYPE)-$(shell date -u +%Y%m%d)
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    BUILD_DISPLAY_ID=$(BUILD_ID) \
-    pearl.ota.version=$(PRODUCT_VERSION_MAJOR)-$(PRODUCT_VERSION_MAINTENANCE) \
-    ro.pearl.version=$(PEARL_XTRA_VERSION) \
-    ro.modversion=$(PEARL_MOD_VERSION) \
-    ro.pearl.buildtype=$(PEARL_BUILD_TYPE) \
-    ro.pearl.display.version=$(PEARL_DISPLAY_VERSION) \
-    com.pearl.fingerprint=$(PEARL_XTRA_VERSION)
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+  ro.pearl.version=$(PEARL_VERSION) \
+  ro.pearl.releasetype=$(PEARL_BUILD_TYPE) \
+  ro.mod.version=$(PEARL_MOD_VERSION)
+
+PEARL_DISPLAY_VERSION := PearlOS-$(PEARL_MOD_VERSION)-$(PEARL_BUILD_TYPE)
+
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+  ro.pearl.display.version=$(PEARL_DISPLAY_VERSION)
