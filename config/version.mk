71f605319818d3a7313bac8bc2f0a69c24e860e8
Sagarrokade006 <sagarrokade006@gmail.com>
2019-01-09 11:19:05 +0000

    Update Versioning System

--- config/version.mk
+++ config/version.mk
@@ -4,8 +4,33 @@ PRODUCT_VERSION_MAINTENANCE = SparkyWhite
 PEARL_POSTFIX := $(shell date +"%Y%m%d")
 PEARL_BUILD_EXTRA := By-Team-Pearl
 
+#UNOFFICIAL
 ifndef PEARL_BUILD_TYPE
     PEARL_BUILD_TYPE := UNOFFICIAL
+endif
+
+# Beta Tag
+ifeq ($(PEARL_BETA),true)
+    PEARL_BUILD_TYPE := BETA
+endif
+
+# Alpha Tag
+ifeq ($(PEARL_ALPHA),true)
+   PEARL_BUILD_TYPE := ALPHA
+endif
+
+# Specific Official Tag
+CURRENT_DEVICE=$(shell echo "$(TARGET_PRODUCT)" | cut -d'_' -f 2,3)
+
+ifeq ($(PEARL_OFFICIAL),true)
+   LIST = $(shell curl -s https://raw.githubusercontent.com/PearlOS/vendor_pearl/pie/pearl.devices)
+   FOUND_DEVICE = $(filter $(CURRENT_DEVICE), $(LIST))
+   ifeq ($(FOUND_DEVICE),$(CURRENT_DEVICE))
+      IS_OFFICIAL=true
+      PEARL_BUILD_TYPE := OFFICIAL
+   else
+      PEARL_BUILD_TYPE := UNOFFICIAL
+   endif
 endif
 
 # Set all versions
