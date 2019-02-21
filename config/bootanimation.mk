572c2c7d2b58a6975ed5b6ffaeee28e32706e3f3
Sagarrokade006 <sagarrokade006@gmail.com>
2019-02-21 13:00:35 +0000

    Add Pearl BootAnimation

--- config/bootanimation.mk
+++ config/bootanimation.mk
@@ -17,11 +17,11 @@
 TARGET_BOOT_ANIMATION_RES ?= undefined
 
  ifeq ($(TARGET_BOOT_ANIMATION_RES),720)
-     PRODUCT_COPY_FILES += vendor/pearl/prebuilt/common/media/LEAN-720.zip:system/media/bootanimation.zip
+     PRODUCT_COPY_FILES += vendor/pearl/prebuilt/common/media/PEARL.zip:system/media/bootanimation.zip
 else ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
-     PRODUCT_COPY_FILES += vendor/pearl/prebuilt/common/media/LEAN-1080.zip:system/media/bootanimation.zip
+     PRODUCT_COPY_FILES += vendor/pearl/prebuilt/common/media/PEARL.zip:system/media/bootanimation.zip
 else ifeq ($(TARGET_BOOT_ANIMATION_RES),1440)
-     PRODUCT_COPY_FILES += vendor/pearl/prebuilt/common/media/LEAN-1440.zip:system/media/bootanimation.zip
+     PRODUCT_COPY_FILES += vendor/pearl/prebuilt/common/media/PEARL.zip:system/media/bootanimation.zip
 else ifeq ($(TARGET_BOOT_ANIMATION_RES),undefined)
      $(warning Target bootanimation res is undefined, using generic bootanimation )
      PRODUCT_COPY_FILES += vendor/pearl/prebuilt/common/media/bootanimation.zip:system/media/bootanimation.zip
